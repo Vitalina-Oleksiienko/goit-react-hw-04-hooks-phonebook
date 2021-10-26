b5bb3d7490a93de4d1e5146a8fb1f67ed086c602
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-26 11:14:30 +0300

    Update ContactList.js

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import style from "./contacts.module.css";
 
-class ContactList extends Component {
-  render() {
+export default function ContactList({ contacts, handleDelete }) {
     return (
       <ul className={style.list}>
-        {this.props.contacts.map((el) => (
+        {contacts.map((el) => (
           <li className={style.listItem} key={el.id}>
             {el.name}: {el.number}
             <button
               type="button"
               className={style.btnDelete}
               id={el.id}
-              onClick={this.props.handleDelete}
+              onClick={handleDelete}
             >
               Delete
             </button>
@@ -22,9 +21,6 @@ class ContactList extends Component {
       </ul>
     );
   }
-}
-
-export default ContactList;
 
 ContactList.propTypes = {
   contacts: PropTypes.array.isRequired,
