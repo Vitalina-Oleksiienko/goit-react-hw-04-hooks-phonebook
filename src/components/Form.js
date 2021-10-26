85706b7fecc90be62c371fe838e39b331a566e87
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-26 11:25:13 +0300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,62 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import style from "./contacts.module.css";
 import { v4 as uuidv4 } from "uuid";
 
-class Form extends Component {
-  state = {
-    name: "",
-    number: "",
+export default function Form({ handleSubmit }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState('');
+
+  const inputNameId = uuidv4();
+  const inputNumberId = uuidv4();
+  
+  const handleChangeInput = (e) => {
+   if (e.target.name === 'name') {
+      setName(e.target.value);
+    } else if (e.target.name === 'number') {
+      setNumber(e.target.value);
+    }
   };
 
-  inputNameId = uuidv4();
-  inputNumberId = uuidv4();
-
-  handleChangeInput = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  render() {
     return (
       <>
         <form
           className={style.form}
           onSubmit={(e) => {
             e.preventDefault();
-            this.props.handleSubmit(this.state);
-            this.setState({
-              name: "",
-              number: "",
-            });
+            handleSubmit(name, number);
+            setName('');
+            setNumber('');
           }}
         >
-          <label htmlFor={this.inputNameId}>Name</label>
+          <label htmlFor={inputNameId}>Name</label>
           <input
             className={style.input}
-            id={this.inputNameId}
-            value={this.state.name}
+            id={inputNameId}
+            value={name}
             autoComplete="off"
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
-            onChange={this.handleChangeInput}
+            onChange={handleChangeInput}
           />
-          <label htmlFor={this.inputNumberId}>Number</label>
+          <label htmlFor={inputNumberId}>Number</label>
           <input
             className={style.input}
-            id={this.inputNumberId}
-            value={this.state.number}
+            id={inputNumberId}
+            value={number}
             autoComplete="off"
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
-            onChange={this.handleChangeInput}
+            onChange={handleChangeInput}
           />
           <button type="submit" className={style.btn}>
             Add contact
@@ -65,9 +62,7 @@ class Form extends Component {
       </>
     );
   }
-}
 
-export default Form;
 
 Form.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
